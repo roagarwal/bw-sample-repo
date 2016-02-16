f301bee0345e62beed7ddafb410387c095137a7e
unknown <roagarwa@roagarwa-t440.apac.tibco.com>
2016-02-16 16:47:58 +0530

    First commit

---
+++ bw6-maven-plugin/src/main/java/com/tibco/plugins/bw6_maven_plugin/App.java
@@ -0,0 +1,13 @@
+package com.tibco.plugins.bw6_maven_plugin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
